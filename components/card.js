03292f4193a33d2c5592c4b694f47825745623f6
Thompson Plyler <nealok.g@gmail.com>
2021-10-16 13:28:53 -0400

    Style changes for danger cards.

--- components/card.js
+++ components/card.js
@@ -1,6 +1,40 @@
 import {useState, useEffect} from 'react'
 import styles from '../styles/card.module.scss'
 
+const alpha = .9
+const regionColorKey = {
+  "Bilgewater": {
+    color: `rgba(113, 38, 28,${alpha} )`
+  },
+  "Piltover & Zaun":{
+    color: `rgba(230, 179, 81,${alpha} )`
+  },
+  "Freljord":{
+    color: `rgba(151, 210, 225,${alpha} )`
+  },
+  "Noxus":{
+    color: `rgba(131, 32, 30,${alpha} )`
+  },
+  "Shurima":{
+    color: `rgba(237, 208, 46,${alpha} )`
+  },
+  "Targon":{
+    color: `rgba(57, 52, 129,${alpha} )`
+  },
+  "Bandle City":{
+    color: `rgba(167, 190, 13,${alpha} )`
+  },
+  "Ionia":{
+    color: `rgba(202, 118, 146,${alpha} )`
+  },
+  "Shadow Isles":{
+    color: `rgba(7, 162, 128,${alpha} )`
+  },
+  "Demacia":{
+    color: `rgba(196, 182, 139,${alpha} )`
+  }
+}
+
 
 export default function Card( {
   name, 
@@ -34,12 +68,25 @@ export default function Card( {
 
   const handleMouseMove = (e) => {
     setModalMousePos({x:e.pageX,y:e.pageY})  }
-    console.log(cost)
+    const color = regionColorKey[`${region}`].color
+    console.log(`The card ${name} has a region of ${region}, which uses the color, ${color}`)
+
 
     return (
             <div className={styles.cardContainer} onMouseEnter={(e)=>handleMouseEnter(e,set,cardCode)} onMouseLeave={handleMouseLeave}>
+              <div style={{background: `linear-gradient(90deg, ${regionColorKey[`${region}`].color} 66%, rgba(9,9,121,0) 83%)`}}>
               <div className={styles.cardDataContainer}>
-                <div className={styles.cardDataCost}>{cost}</div><div className={styles.cardText}>{name}</div>
+                
+              <div 
+                className={styles.costImageContainer}
+              >
+                    <p className={styles.costText}>{cost}</p>
+                    <img className={styles.costImage} src={`/images/ui-images/Empty_Mana_Art_LoR.png`}/>
+              </div>
+
+
+                <div className={styles.cardText}>{name}</div>
+              </div>
               </div>
               <img className={styles.cardImage} src={`/lorData/${set}/en_us/img/cards/${cardCode}.png`}/>
             </div>
